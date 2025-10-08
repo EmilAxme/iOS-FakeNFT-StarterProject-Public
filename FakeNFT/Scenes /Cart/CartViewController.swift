e92294d779297cc4d7af26f67ab806ff478cf107
EmilAxme <emil.azmedov@mail.ru>
2025-10-09 00:27:25 +0300

    Добавил private lazy var CollectionView и вынес в extention dataSource и delegateFlowLayout

--- FakeNFT/Scenes /Cart/CartViewController.swift
+++ FakeNFT/Scenes /Cart/CartViewController.swift
@@ -9,4 +9,27 @@ import UIKit
 
 final class CartViewController: UIViewController {
     
+    private lazy var nftCollectionView: UICollectionView = {
+        var collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        return collectionView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
+
+extension CartViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+    }
+}
+
+extension CartViewController: UICollectionViewDelegateFlowLayout {
+    
 }
