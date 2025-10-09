b4023671eac42aa5dd22a1303096a0f282e4e1d3
EmilAxme <emil.azmedov@mail.ru>
2025-10-09 12:17:58 +0300

    добавил presenter,  реализовал минимум для верстки экрана коллекции

--- FakeNFT/Scenes /Cart/CartPresenter.swift
+++ FakeNFT/Scenes /Cart/CartPresenter.swift
@@ -5,3 +5,32 @@
 //  Created by Emil on 09.10.2025.
 //
 
+import Foundation
+import UIKit
+
+protocol CartViewProtocol: AnyObject {
+    func reloadData()
+}
+
+protocol CartPresenterProtocol: AnyObject {
+    var nfts: [NFTMock] { get }
+    func viewDidLoad()
+}
+
+final class CartPresenter: CartPresenterProtocol {
+    weak var view: CartViewProtocol?
+    private(set) var nfts: [NFTMock] = []
+    
+    init(view: CartViewProtocol?) {
+        self.view = view
+    }
+    
+    func viewDidLoad() {
+        nfts = [
+            NFTMock(name: "April", price: 1.78, rating: 1, image: UIImage(resource: .nft1)),
+            NFTMock(name: "Greena", price: 1.78, rating: 3, image: UIImage(resource: .nft2)),
+            NFTMock(name: "Spring", price: 1.78, rating: 5, image: UIImage(resource: .nft3))
+        ]
+        view?.reloadData()
+    }
+}
