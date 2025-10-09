9682b739cc19af66fce421827cd3469fbfe378ba
EmilAxme <emil.azmedov@mail.ru>
2025-10-10 00:45:12 +0300

    Изменил layout через отдельную функцию collectionView

--- FakeNFT/Scenes /Cart/CartViewController.swift
+++ FakeNFT/Scenes /Cart/CartViewController.swift
@@ -9,27 +9,72 @@ import UIKit
 
 final class CartViewController: UIViewController {
     
+    private var presenter: CartPresenterProtocol?
+    
     private lazy var nftCollectionView: UICollectionView = {
-        var collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 0
+        layout.sectionInset = .zero
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .systemBackground
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(NFTCollectionViewCell.self, forCellWithReuseIdentifier: NFTCollectionViewCell.reuseIdentifier)
         return collectionView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        presenter = CartPresenter(view: self)
+        setupUI()
+        presenter?.viewDidLoad()
+    }
+    
+    private func setupUI() {
+        view.addSubview(nftCollectionView)
+        nftCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            nftCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            nftCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            nftCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            nftCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
 }
 
 extension CartViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
+        presenter?.nfts.count ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NFTCollectionViewCell.reuseIdentifier, for: indexPath) as? NFTCollectionViewCell,
+            let nft = presenter?.nfts[indexPath.item]
+        else {
+            return UICollectionViewCell()
+        }
         
+        cell.configure(with: nft)
+        return cell
     }
 }
 
 extension CartViewController: UICollectionViewDelegateFlowLayout {
-    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.bounds.width
+        return CGSize(width: width, height: 140)
+    }
+}
+
+extension CartViewController: CartViewProtocol {
+    func reloadData() {
+        nftCollectionView.reloadData()
+    }
 }
