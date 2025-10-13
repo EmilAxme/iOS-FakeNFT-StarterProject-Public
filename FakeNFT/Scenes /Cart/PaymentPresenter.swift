76610c3237442de43f3acf3253d2b066e2a4df0d
EmilAxme <emil.azmedov@mail.ru>
2025-10-13 21:39:09 +0300

    добавил presenter для PaymentViewController

---
+++ FakeNFT/Scenes /Cart/PaymentPresenter.swift
@@ -0,0 +1,40 @@
+//
+//  PaymentPresenter.swift
+//  FakeNFT
+//
+//  Created by Emil on 13.10.2025.
+//
+
+import UIKit
+
+protocol PaymentPresenterProtocol: AnyObject {
+    var currencies: [CurrencyModel] { get }
+    func viewDidLoad()
+    func didTapPayButton()
+}
+
+final class PaymentPresenter: PaymentPresenterProtocol {
+    weak var view: PaymentViewController?
+    var currencies: [CurrencyModel] = []
+    
+    init(view: PaymentViewController?) {
+        self.view = view
+    }
+    
+    func viewDidLoad() {
+        currencies = [
+            CurrencyModel(name: "Bitcoin", ticker: "BTC", icon: UIImage(resource: .bitcoin)),
+            CurrencyModel(name: "Dogecoin", ticker: "DOGE", icon: UIImage(resource: .dogecoin)),
+            CurrencyModel(name: "Tether", ticker: "USDT", icon: UIImage(resource: .tether)),
+            CurrencyModel(name: "Apecoin", ticker: "APE", icon: UIImage(resource: .apecoin)),
+            CurrencyModel(name: "Solana", ticker: "SOL", icon: UIImage(resource: .solana)),
+            CurrencyModel(name: "Ethereum", ticker: "ETH", icon: UIImage(resource: .ethereum)),
+            CurrencyModel(name: "Cardano", ticker: "ADA", icon: UIImage(resource: .cardano)),
+            CurrencyModel(name: "Shiba Inu", ticker: "SHIB", icon: UIImage(resource: .shibaInu))]
+        view?.reloadData()
+    }
+    
+    func didTapPayButton() {
+        print("Оплата началась")
+    }
+}
