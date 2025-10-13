ac4fb555bad9947249f92e0deccde2a4d9e34a3f
EmilAxme <emil.azmedov@mail.ru>
2025-10-13 19:14:38 +0300

    Сверстал элементы navigationBar

--- FakeNFT/Scenes /Cart/PaymentViewController.swift
+++ FakeNFT/Scenes /Cart/PaymentViewController.swift
@@ -10,5 +10,25 @@ import UIKit
 final class PaymentViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        title = "Выберите способ оплаты"
+        navigationController?.navigationBar.prefersLargeTitles = false
+        
+        let backImage = UIImage(resource: .backward)
+        let backButton = UIBarButtonItem(
+            image: backImage,
+            style: .plain,
+            target: self,
+            action: #selector(backButtonTapped)
+        )
+        
+        navigationItem.leftBarButtonItem = backButton
+    }
+    
+    @objc private func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
     }
 }
