2229735796435620455cf9ce180d35022c6b1f36
EmilAxme <emil.azmedov@mail.ru>
2025-10-13 15:52:10 +0300

    создал PaymentViewController

---
+++ FakeNFT/Scenes /Cart/PaymentViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PaymentViewController.swift
+//  FakeNFT
+//
+//  Created by Emil on 13.10.2025.
+//
+
+import UIKit
+
+final class PaymentViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
