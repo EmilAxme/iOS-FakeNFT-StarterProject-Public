9dd13148ff1eb7c434069efa51969c0c6a52f006
EmilAxme <emil.azmedov@mail.ru>
2025-10-21 09:26:14 +0300

    реализовал новый роутер перехода на экран успешной оплаты

--- FakeNFT/Scenes /Cart/Payment/PaymentRouter.swift
+++ FakeNFT/Scenes /Cart/Payment/PaymentRouter.swift
@@ -1,1 +1,26 @@
+//
+//  PaymentRouter.swift
+//  FakeNFT
+//
+//  Created by Emil on 14.10.2025.
+//
+
+import UIKit
+
+protocol PaymentRouterProtocol {
+    func openPaymentSuccess()
+}
+
+final class PaymentRouter: PaymentRouterProtocol {
+    weak var viewController: UIViewController?
+    
+    init(viewController: UIViewController?) {
+        self.viewController = viewController
+    }
+    
+    func openPaymentSuccess() {
+        let successVC = PaymentSuccessViewController()
+        viewController?.navigationController?.pushViewController(successVC, animated: true)
+    }
+}
 
