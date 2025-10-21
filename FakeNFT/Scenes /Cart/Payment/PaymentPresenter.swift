a87fe3d3198f256de1214828af96b94f77b24bb9
EmilAxme <emil.azmedov@mail.ru>
2025-10-21 19:58:04 +0300

    написал метод загрузки currency из апишки в presenter

--- FakeNFT/Scenes /Cart/Payment/PaymentPresenter.swift
+++ FakeNFT/Scenes /Cart/Payment/PaymentPresenter.swift
@@ -31,7 +31,26 @@ final class PaymentPresenter: PaymentPresenterProtocol {
     }
     
     func viewDidLoad() {
+        view?.showLoading()
+        loadCurrencies()
         view?.reloadData()
+    }
+    
+    private func loadCurrencies() {
+        currencyService.loadCurrencies { [weak self] result in
+            guard let self else { return }
+
+            switch result {
+            case .success(let currencies):
+                self.currencies = currencies
+                self.view?.reloadData()
+                self.view?.hideLoading()
+            case .failure(let error):
+                print("❌ Ошибка загрузки валют: \(error.localizedDescription)")
+                self.view?.hideLoading()
+                self.view?.showPaymentErrorAlert()
+            }
+        }
     }
     
     func didTapPayButton() {
