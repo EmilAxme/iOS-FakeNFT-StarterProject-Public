80b27a62dd7980d4161706ea8a0e6d0dbde4e66d
EmilAxme <emil.azmedov@mail.ru>
2025-10-21 19:54:41 +0300

    убрал mock валюту из viewDidLoad

--- FakeNFT/Scenes /Cart/Payment/PaymentPresenter.swift
+++ FakeNFT/Scenes /Cart/Payment/PaymentPresenter.swift
@@ -28,15 +28,6 @@ final class PaymentPresenter: PaymentPresenterProtocol {
     }
     
     func viewDidLoad() {
-        currencies = [
-            CurrencyModel(name: "Bitcoin", ticker: "BTC", icon: UIImage(resource: .bitcoin)),
-            CurrencyModel(name: "Dogecoin", ticker: "DOGE", icon: UIImage(resource: .dogecoin)),
-            CurrencyModel(name: "Tether", ticker: "USDT", icon: UIImage(resource: .tether)),
-            CurrencyModel(name: "Apecoin", ticker: "APE", icon: UIImage(resource: .apecoin)),
-            CurrencyModel(name: "Solana", ticker: "SOL", icon: UIImage(resource: .solana)),
-            CurrencyModel(name: "Ethereum", ticker: "ETH", icon: UIImage(resource: .ethereum)),
-            CurrencyModel(name: "Cardano", ticker: "ADA", icon: UIImage(resource: .cardano)),
-            CurrencyModel(name: "Shiba Inu", ticker: "SHIB", icon: UIImage(resource: .shibaInu))]
         view?.reloadData()
     }
     
