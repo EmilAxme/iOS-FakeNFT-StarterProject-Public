402faddd1e7d6454f535d8b6c96a274ad2f200b6
EmilAxme <emil.azmedov@mail.ru>
2025-10-24 10:09:42 +0300

    проинициализировал paymentService в paymentPresenter

--- FakeNFT/Scenes /Cart/Payment/PaymentPresenter.swift
+++ FakeNFT/Scenes /Cart/Payment/PaymentPresenter.swift
@@ -18,16 +18,18 @@ final class PaymentPresenter: PaymentPresenterProtocol {
     private let router: PaymentRouterProtocol
     private let cartService: CartServiceProtocol
     private let currencyService: CurrencyServiceProtocol
+    private let paymentService: PaymentServiceProtocol
     
     var currencies: [CurrencyModel] = []
-
     private var hasFailedOnce = false
+    private var selectedCurrencyId: String?
     
-    init(view: PaymentViewController?, cartService: CartServiceProtocol = CartService.shared, router: PaymentRouterProtocol, currencyService: CurrencyServiceProtocol = CurrencyService()) {
+    init(view: PaymentViewController?, cartService: CartServiceProtocol = CartService.shared, router: PaymentRouterProtocol, currencyService: CurrencyServiceProtocol = CurrencyService(), paymentService: PaymentServiceProtocol = PaymentService()) {
         self.view = view
         self.router = router
         self.cartService = cartService
         self.currencyService = currencyService
+        self.paymentService = paymentService
     }
     
     func viewDidLoad() {
